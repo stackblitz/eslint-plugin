c0952e11c8ef88f213c4d9f67b4e14553639a4ca
Joan Varvenne <Nemikolh@users.noreply.github.com>
2023-01-13 08:39:59 +0000

    chore: add json test for comment-syntax

--- test/utils/index.ts
+++ test/utils/index.ts
@@ -1,4 +1,5 @@
-import { ESLintUtils } from '@typescript-eslint/utils';
+import { RuleTester } from 'eslint';
+import { ESLintUtils, TSESLint } from '@typescript-eslint/utils';
 
 export function ruleTester() {
   return new ESLintUtils.RuleTester({
@@ -8,4 +9,19 @@ export function ruleTester() {
       sourceType: 'module',
     },
   });
+}
+
+export function ruleTesterForJSONC() {
+  return new RuleTester({
+    parser: require.resolve('jsonc-eslint-parser'),
+  }) as unknown as {
+    run<TMessageIds extends string, TOptions extends Readonly<unknown[]>>(
+      name: string,
+      rule: TSESLint.RuleModule<TMessageIds, TOptions>,
+      tests: {
+        valid?: Array<string | RuleTester.ValidTestCase> | undefined;
+        invalid?: RuleTester.InvalidTestCase[] | undefined;
+      }
+    ): void;
+  };
 }
